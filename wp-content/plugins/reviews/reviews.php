defaa0bf08438c110fb4c561943c4cd048832a42
Dillon Kavanagh <dillon@viralbamboo.com>
2018-03-15 15:09:40 +0000

    Working migrations on Activate / Deactivate

--- wp-content/plugins/reviews/reviews.php
+++ wp-content/plugins/reviews/reviews.php
@@ -3,12 +3,17 @@
 Plugin Name: Reviews
 */
 
+// include('wp-list-table.php');
+// include(plugin_dir_path(__FILE__) . 'wp-list-table.php');
+
+if (! class_exists('WP_List_Table')) {
+    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+}
+
 
 # Activation and deactivation hooks provide ways to perform actions when plugins are activated or deactivated.
 
 register_activation_hook(__FILE__, 'createTable');
-
-
 
 function createTable()
 {
@@ -19,11 +24,10 @@ function createTable()
     id mediumint(9) NOT NULL AUTO_INCREMENT,
     product varchar(191) DEFAULT '' NOT NULL,
     full_name varchar(191) DEFAULT '' NOT NULL,
-    full_name varchar(191) DEFAULT '' NOT NULL,
     email varchar(191) DEFAULT '' NOT NULL,
     title varchar(191) DEFAULT '' NOT NULL,
     description varchar(191) DEFAULT '' NOT NULL,
-    rating mediumint(9) NOT NULL,
+    rating tinyint(9) DEFAULT 0 NOT NULL,
     is_approved tinyint(1) DEFAULT 0 NOT NULL,
     time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
     PRIMARY KEY  (id)
@@ -42,5 +46,18 @@ function dropTable()
     $table_name = $wpdb->prefix . "reviews";
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     $wpdb->query("DROP TABLE IF EXISTS $table_name");
-    dbDelta($sql);
 }
+
+# Create a Table for Review Management
+
+class Review_List_Table extends WP_List_Table
+{
+    public function get_columns()
+    {
+    }
+    public function prepare_items()
+    {
+    }
+}
+
+// $reviewList = new Review_List_Table();
